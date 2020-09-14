de9a9d1386cb821f0506ca786d3350751f6e60c6
Ben Nguyen <nyetex@gmail.com>
2020-09-14 09:39:52 -0700

    revert config back to 1.4.1 defaults

--- app/config.js
+++ app/config.js
@@ -6,58 +6,29 @@ const set = require('lodash/set')
 const electron = require('electron')
 const appRootDir = require('app-root-dir')
 const getPlatform = require('./get-platform')
-const { dialog } = require('electron')
+
 const app = electron.app || electron.remote.app
+
 const isProd = process.env.NODE_ENV === 'production'
+
 const daemonPath = isProd
   ? path.join(process.resourcesPath, 'bin')
   : path.join(appRootDir.get(), 'bin', getPlatform())
-  
-  //Multi platform install
-  var platform = process.platform; 
-  switch(platform) { 
-    case 'win32':  
-	newcPath = path.join(app.getPath('home'), '/Appdata/Local/ScPrime')
-        break; 
-	
-    case 'darwin':  
-    newcPath = path.join(app.getPath('home'), '/Library/Application Support/ScPrime')
-        break; 
-		
-	 default:  
-    newcPath = path.join(app.getPath('home'), './ScPrime')
-        break; 
-		 
-} 
-  
-   //Try to move metadata
-    newcPathExists = fs.existsSync(newcPath)
-    if (!newcPathExists) {
-        currentcPath = path.join(app.getPath('userData'), './siaprime')
-        currentcPathExists = fs.existsSync(currentcPath)
-        if (currentcPathExists) {
-              try {
-                 fs.renameSync(currentcPath, newcPath)
-             } catch (err) {
-          message = 'Close any open folders, ensure the program is no longer running and try again\n\n' + err
-          dialog.showErrorBox('Error moving/renaming directories', message)
-          app.exit()
-         }
-     }
-  }
+
 // Setup the default path for Siad
 const defaultSiadPath = path.join(
   daemonPath,
   `${process.platform === 'win32' ? 'spd.exe' : 'spd'}`
 )
+
 const defaultSiacPath = path.join(
   daemonPath,
   `${process.platform === 'win32' ? 'spc.exe' : 'spc'}`
 )
-// User config path
-const userConfigFolder = newcPath
-const userConfigPath = path.join(userConfigFolder, 'config.json')
 
+// User config path
+const userConfigFolder = path.join(app.getPath('userData'), 'scprime')
+const userConfigPath = path.join(userConfigFolder, 'config.json')
 // Default config
 let defaultConfig = {
   darkMode: false,
@@ -66,7 +37,7 @@ let defaultConfig = {
   siad: {
     useCustomBinary: false,
     path: defaultSiadPath,
-    datadir: newcPath
+    datadir: path.join(app.getPath('userData'), './scprime')
   },
   siac: {
     useCustomBinary: false,
@@ -75,6 +46,7 @@ let defaultConfig = {
   logPath: userConfigFolder,
   userConfigPath
 }
+
 let userConfig
 try {
   const userConfigBuffer = fs.readFileSync(userConfigPath)
@@ -90,6 +62,7 @@ try {
 } catch (err) {
   console.error('error reading user config file:', err)
 }
+
 try {
   if (!isEqual(userConfig, defaultConfig)) {
     fs.writeFileSync(userConfigPath, JSON.stringify(defaultConfig, null, 4))
@@ -97,4 +70,5 @@ try {
 } catch (err) {
   console.error('error saving user config file:', err)
 }
+
 module.exports = defaultConfig
