b51dbd86876934572fa1b45024d59b578f43797a
Ben Nguyen <nyetex@gmail.com>
2020-10-29 10:36:20 -0700

    removed brackets from merge

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -5,7 +5,7 @@
 const path = require('path')
 const webpack = require('webpack')
 // const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const { merge }= require('webpack-merge')
+const  merge = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const baseConfig = require('./webpack.config.base')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
